4dd2eba42dbb1b9ad4db803b8cb1ad1ccacf1a38
Igor Parra <igorparrabastias@gmail.com>
2024-03-16 18:41:20 -0300

    Update

--- notebook/arboles/src/BinaryTree.py
+++ notebook/arboles/src/BinaryTree.py
@@ -1,34 +1,34 @@
 class Node:
     def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.data = key
+        self.left = None          # Inicializa el hijo izquierdo como None
+        self.right = None         # Inicializa el hijo derecho como None
+        self.data = key           # Establece el valor de datos para el nodo
 
 class BinaryTree:
     def __init__(self):
-        self.root = None
+        self.root = None          # Inicializa el nodo raíz como None al crear una nueva instancia de Árbol Binario
 
     def insert(self, key):
-        new_node = Node(key)
-        if not self.root:
-            self.root = new_node
-            return
+        new_node = Node(key)      # Crea un nuevo nodo con la clave proporcionada
+        if not self.root:         # Si el árbol está vacío,
+            self.root = new_node  # establece el nuevo nodo como la raíz y devuelve
 
-        queue = [self.root]
+        queue = [self.root]       # Crea una cola comenzando con el nodo raíz
         while queue:
-            current = queue.pop(0)
+            current = queue.pop(0)   # Desencola el elemento frontal de la cola
 
-            # Si el nodo actual no tiene hijo izquierdo, lo insertamos aquí.
+            # Si el nodo actual no tiene un hijo izquierdo, inserta el nuevo nodo aquí
             if not current.left:
                 current.left = new_node
                 return
             else:
-                queue.append(current.left)
+                queue.append(current.left)  # Agrega el hijo izquierdo a la cola
 
-            # Si el nodo actual no tiene hijo derecho, lo insertamos aquí.
+            # Si el nodo actual no tiene un hijo derecho, inserta el nuevo nodo aquí
             if not current.right:
                 current.right = new_node
                 return
             else:
-                queue.append(current.right)
+                queue.append(current.right)  # Agrega el hijo derecho a la cola
+
 
