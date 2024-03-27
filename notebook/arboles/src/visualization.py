872a9975dc118d25ab2769438f33a847b4ece07a
Igor Parra <igorparrabastias@gmail.com>
2024-03-27 11:36:32 -0300

    Update

--- notebook/arboles/src/visualization.py
+++ notebook/arboles/src/visualization.py
@@ -51,3 +51,46 @@ def visualize_t(tree):
     add_nodes_edges(tree.root, dot=dot)
 
     return dot
+
+def visualize_rbtree(tree):
+    dot = Digraph(comment='Red-Black Tree')
+
+    if not tree.root or tree.root == tree.NIL:
+        dot.node('null', 'Empty Tree', shape='plaintext')
+        return dot
+
+    def add_nodes_edges(node, dot=None, parent_id=None, edge_label=''):
+        if node == tree.NIL:
+            nil_id = f"nil{parent_id}{edge_label}"
+            dot.node(nil_id, 'NIL', shape='box', style='filled', fillcolor='grey', fontcolor='white')
+            dot.edge(parent_id, nil_id, label=edge_label)
+            return
+
+        node_id = str(id(node))  # Identificador único basado en la id de memoria del nodo
+        node_label = f"{node.data}\n{node.color.upper()}"
+
+        # Ajusta el color del fondo y de la fuente según el color del nodo
+        if node.color == "red":
+            dot.node(node_id, node_label, style='filled', fillcolor='white', fontcolor='black')
+        else:  # Nodo negro
+            dot.node(node_id, node_label, style='filled', fillcolor='black', fontcolor='white')
+
+        # Si el nodo no es la raíz, añade un borde al padre
+        if parent_id is not None:
+            dot.edge(parent_id, node_id, label=edge_label)
+
+        add_nodes_edges(node.left, dot=dot, parent_id=node_id, edge_label='L')
+        add_nodes_edges(node.right, dot=dot, parent_id=node_id, edge_label='R')
+
+    # Inicia el proceso con el nodo raíz
+    root_id = str(id(tree.root))
+    root_label = f"{tree.root.data}\n{tree.root.color.upper()}"
+
+    if tree.root.color == "red":
+        dot.node(root_id, root_label, style='filled', fillcolor='white', fontcolor='black')
+    else:  # Nodo raíz negro
+        dot.node(root_id, root_label, style='filled', fillcolor='black', fontcolor='white')
+
+    add_nodes_edges(tree.root, dot=dot, parent_id=root_id)
+
+    return dot
